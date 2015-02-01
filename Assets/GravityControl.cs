e1e4576c53e3c623f30e34aab987f58930e3cfa7
Semenov Anton <allcreater@gmail.com>
2015-02-01 18:57:11 +0300

    Very first commit

---
+++ Assets/GravityControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		Physics.gravity = direction;
+	}
+}
