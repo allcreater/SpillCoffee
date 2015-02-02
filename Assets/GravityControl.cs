4c53e2e3cef50a6a04faad327a843fb7e0cc4ebe
Semenov Anton <allcreater@gmail.com>
2015-02-02 17:22:08 +0300

    Ability to control the gravity, particles tuned up, etc

--- Assets/GravityControl.cs
+++ Assets/GravityControl.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 
 public class GravityControl : MonoBehaviour {
+	private float declinationAngle = 0.0f;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		var direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		Physics.gravity = direction;
+		declinationAngle -= Input.GetAxis ("Horizontal") * Time.deltaTime;
+		declinationAngle = Mathf.Clamp (declinationAngle, -1.0f, 1.0f);
+
+		//var direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		var direction = new Vector3(-Mathf.Sin (declinationAngle), -Mathf.Cos (declinationAngle), 0.0f);
+		Physics.gravity = direction * 9.81f;
 	}
 }
---
+++ Assets/GravityOrientation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityOrientation : MonoBehaviour
+{
+	private Quaternion initialOrientation;
+	// Use this for initialization
+	void Start ()
+	{
+		initialOrientation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = initialOrientation * Quaternion.AngleAxis (Mathf.Atan2 (Physics.gravity.x, -Physics.gravity.y) * 57.3f, new Vector3 (0.0f, 0.0f, 1.0f));
+	}
+}
