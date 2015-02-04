3e7b738542035ce5af76c06d0c5c7bc102a0ebd2
Eugene Pavlov <bjohn.ext@gmail.com>
2015-02-04 19:33:08 +0300

    add moved files back (change folder)

---
+++ Assets/Script/GoToStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToStart : MonoBehaviour
+{
+	public void LoadStartLevel()
+	{
+		Application.LoadLevel ("mainScene");
+		Physics.gravity = new Vector3 (0.0f, -9.81f, 0.0f);
+	}
+}
---
+++ Assets/Script/GravityControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour {
+	private float declinationAngle = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		declinationAngle -= Input.GetAxis ("Horizontal") * Time.deltaTime;
+		declinationAngle = Mathf.Clamp (declinationAngle, -1.0f, 1.0f);
+
+		float noiseDeclination = 
+            Mathf.Sin (Time.realtimeSinceStartup * 3.0f)*0.7f 
+            + Mathf.Sin (Time.realtimeSinceStartup * 1.71f + 2.3f)*0.3f 
+            + Mathf.Sin (Time.realtimeSinceStartup * 4.71f)*0.1f;
+
+        //Debug.Log("axes: " + (declinationAngle + noiseDeclination));
+
+		//var direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		var direction = new Vector3(-Mathf.Sin (declinationAngle + noiseDeclination), -Mathf.Cos (declinationAngle + noiseDeclination), 0.0f);
+		Physics.gravity = direction * 9.81f;
+	}
+}
